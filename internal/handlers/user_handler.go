1677b6a83f4429a500e70c294fff38a7ae44ded5
Alexis Maximiliano Alva Martínez <A01754754@tec.mx>
2026-09-20 16:51:27 -0600

    Logout Function

--- internal/handlers/user_handler.go
+++ internal/handlers/user_handler.go
@@ -203,6 +203,13 @@ func LoginHandler(pool *pgxpool.Pool, cfg *config.Config) gin.HandlerFunc {
 	}
 }
 
+func LogoutHandler(c *gin.Context) {
+	c.SetSameSite(http.SameSiteLaxMode)
+	c.SetCookie("token", "", -1, "/", "localhost", false, true)
+
+	c.JSON(http.StatusOK, gin.H{"message": "Logged out successfully"})
+}
+
 /*HELPERS*/
 
 func respondWithToken(c *gin.Context, status int, userID uuid.UUID, cfg *config.Config, isAdmin bool, userType string, userName string) {
